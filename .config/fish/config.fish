728d45d0d02e0c5c23a6090cb9fcce5ebb4b8fa1
Jeff Sandberg <jeff.sandberg@onelogin.com>
2014-08-20 16:25:11 -0700

    Add git hash to prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -114,6 +114,13 @@ function fish_prompt --description 'Write out the prompt'
     end
   end
 
+  # Get the current Git commit hash
+  function __paradox_git_hash --description "Get the current git commit hash, if we're in a git working tree"
+    if test "true" = (git rev-parse --is-inside-work-tree)
+      echo -s "[" (git rev-parse --short HEAD) "]"
+    end
+  end
+
   switch $USER
 
   case root
@@ -173,7 +180,7 @@ function fish_prompt --description 'Write out the prompt'
     set -l prompt_separator_characters (jot -b "━" -s "" $prompt_separator_width)
     echo -s "$prompt_separator_color" "$prompt_separator_characters"
   end
-  echo -s "$__fish_prompt_user" "$USER" "$__fish_prompt_splitter" @ "$__fish_prompt_host" "$__fish_prompt_hostname" "$__fish_prompt_normal" ' ' "$__fish_prompt_cwd" (prompt_pwd) (__fish_git_prompt) "$__fish_prompt_normal" "$prompt_status"
+  echo -s "$__fish_prompt_user" "$USER" "$__fish_prompt_splitter" @ "$__fish_prompt_host" "$__fish_prompt_hostname" "$__fish_prompt_normal" ' ' "$__fish_prompt_cwd" (prompt_pwd) (__fish_git_prompt) (set_color blue) (__paradox_git_hash) "$__fish_prompt_normal" "$prompt_status"
   echo -s "$__fish_prompt_delim"
   set -g __fish_prompt 1
 end
