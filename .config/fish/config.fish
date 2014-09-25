f43281b26cc05f0b28c7f86047ca971609a7c5ea
Jeff Sandberg <paradox460@gmail.com>
2014-09-25 11:01:50 -0700

    Update git hash command to support new repos
    Previously would have generated a ton of line noise and error messages
    if your git repo doesn't actually have any commits in it. Now it doesn't

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -128,10 +128,12 @@ function fish_prompt --description 'Write out the prompt'
   # Get the current Git commit hash
   function __paradox_git_hash --description "Get the current git commit hash, if we're in a git working tree"
     set -l git_info (command git rev-parse --is-inside-work-tree --short HEAD ^/dev/null)
-    set -l inside_worktree $git_info[1]
-    set -l git_head $git_info[2]
-    if test "true" = "$inside_worktree"
-      echo -s "[$git_head]"
+    if set -q $git_info
+      set -l inside_worktree $git_info[1]
+      set -l git_head $git_info[2]
+      if test "true" = "$inside_worktree"
+        echo -s "[$git_head]"
+      end
     end
   end
 
