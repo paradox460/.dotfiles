d7403210d6485d2a9a31d8bfdc68c080d540d227
Jeff Sandberg <paradox460@gmail.com>
2015-01-07 17:56:56 -0800

    Cleaned up fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -102,34 +102,16 @@ function fish_prompt --description 'Write out the prompt'
   set -l last_status $status
 
   # Just calculate these once, to save a few cycles when displaying the prompt
-  if not set -q __fish_prompt_hostname
-    set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
-  end
-
-  if not set -q __fish_prompt_normal
-    set -g __fish_prompt_normal (set_color normal)
+  if not set -q __paradox_prompt_normal
+    set -g __paradox_prompt_normal (set_color normal)
   end
 
   if not set -q -g __fish_classic_git_functions_defined
     set -g __fish_classic_git_functions_defined
 
-    function __fish_repaint_user --on-variable fish_color_user --description "Event handler, repaint when fish_color_user changes"
-      if status --is-interactive
-        set -e __fish_prompt_user
-        commandline -f repaint ^/dev/null
-      end
-    end
-
-    function __fish_repaint_host --on-variable fish_color_host --description "Event handler, repaint when fish_color_host changes"
-      if status --is-interactive
-        set -e __fish_prompt_host
-        commandline -f repaint ^/dev/null
-      end
-    end
-
     function __fish_repaint_status --on-variable fish_color_status --description "Event handler; repaint when fish_color_status changes"
       if status --is-interactive
-        set -e __fish_prompt_status
+        set -e __paradox_prompt_status
         commandline -f repaint ^/dev/null
       end
     end
@@ -157,48 +139,37 @@ function fish_prompt --description 'Write out the prompt'
 
   case root
 
-    if not set -q __fish_prompt_cwd
+    if not set -q __paradox_prompt_cwd
       if set -q fish_color_cwd_root
-        set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
+        set -g __paradox_prompt_cwd (set_color $fish_color_cwd_root)
       else
-        set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+        set -g __paradox_prompt_cwd (set_color $fish_color_cwd)
       end
     end
-    if not set -q __fish_prompt_delim
-      set -g __fish_prompt_delim '❗ $ '
+    if not set -q __paradox_prompt_delim
+      set -g __paradox_prompt_delim '! '
     end
 
   case '*'
 
-    if not set -q __fish_prompt_cwd
-      set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+    if not set -q __paradox_prompt_cwd
+      set -g __paradox_prompt_cwd (set_color $fish_color_cwd)
     end
 
-    if not set -q __fish_prompt_delim
-      set -g __fish_prompt_delim '🍩 $ '
+    if not set -q __paradox_prompt_delim
+      set -g __paradox_prompt_delim '$ '
     end
 
   end
 
   set -l prompt_status
   if test $last_status -ne 0
-    if not set -q __fish_prompt_status
-      set -g __fish_prompt_status (set_color $fish_color_status)
+    if not set -q __paradox_prompt_status
+      set -g __paradox_prompt_status (set_color $fish_color_status)
     end
-    set prompt_status "$__fish_prompt_status [$last_status]$__fish_prompt_normal"
+    set prompt_status "$__paradox_prompt_status [$last_status]$__paradox_prompt_normal"
   end
 
-  if not set -q __fish_prompt_user
-    set -g __fish_prompt_user (set_color $fish_color_user)
-  end
-  if not set -q __fish_prompt_host
-    set -g __fish_prompt_host (set_color $fish_color_host)
-  end
-  if not set -q __fish_prompt_splitter
-    set -g __fish_prompt_splitter (set_color $fish_color_splitter)
-  end
-
-  set -l __fish_prompt_command_count "$__fish_command_count"
 
   # Separator
   set -l prompt_separator_color
@@ -208,13 +179,13 @@ function fish_prompt --description 'Write out the prompt'
     set prompt_separator_color (set_color green)
   end
   set -l prompt_separator_width (math $COLUMNS - 1)
-  if set -q __fish_prompt
+  if set -q __paradox_prompt
     set -l prompt_separator_characters (command jot -b "━" -s "" $prompt_separator_width ^/dev/null)
     echo -s "$prompt_separator_color" "$prompt_separator_characters"
   end
-  echo -s "$__fish_prompt_user" "$USER" "$__fish_prompt_splitter" @ "$__fish_prompt_host" "$__fish_prompt_hostname" "$__fish_prompt_normal" ' ' "$__fish_prompt_cwd" (prompt_pwd) (__fish_git_prompt) (set_color blue) (__paradox_git_hash) "$__fish_prompt_normal" "$prompt_status"
-  echo -s "$__fish_prompt_delim"
-  set -g __fish_prompt 1
+  echo -s "$__paradox_prompt_cwd" (prompt_pwd) (__fish_git_prompt) (set_color blue) (__paradox_git_hash) "$__paradox_prompt_normal" "$prompt_status"
+  echo -s "$__paradox_prompt_delim"
+  set -g __paradox_prompt 1
 end
 
 # initialize our new variables
