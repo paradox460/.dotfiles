e79c7739eeb91fd83f22f0e21f488c0de981b33c
Jeff Sandberg <paradox460@gmail.com>
2015-09-29 14:04:02 -0700

    Add timeout to git hash function for safety

--- .config/fish/functions/paradox_git_hash.fish
+++ .config/fish/functions/paradox_git_hash.fish
@@ -1,5 +1,11 @@
 function paradox_git_hash --description "Get the current git commit hash, if we're in a git working tree"
-  set -l git_info (command git rev-parse --is-inside-work-tree --short HEAD ^/dev/null)
+  set -l git_command 'git rev-parse --is-inside-work-tree --short HEAD ^/dev/null'
+  
+  if type gtimeout >/dev/null ^/dev/null
+    set  -l git_command 'timeout 1' git_command
+  end
+
+  set -l git_info (eval $git_command)
   if test (count $git_info) -gt 1
     set -l inside_worktree $git_info[1]
     set -l git_head $git_info[2]
