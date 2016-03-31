b543d9b8d5db92995f6081830749da00566d9b9c
Jeff Sandberg <jeffsandberg@Jeffs-MacBook-Pro.local>
2016-03-31 12:38:29 -0700

    Make hammerspoon window notifications more persistent.
    Unfortunately, this means you'll have to clean them out of your notification center from time to time. But that place is a shithole anyways.

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -6,11 +6,11 @@ local super = {'ctrl', 'alt', 'cmd', 'shift'}
 function screensChangedCallback()
   local screens = hs.screen.allScreens()
   if #screens < 2 then
-    hs.notify.show('Switched KWM modes', '', 'Switched to floating window mode',''):withdraw()
+    hs.notify.show('Switched KWM modes', '', 'Switched to floating window mode','')
     hs.execute('kwmc space -t float', true)
     hs.execute('kwmc config focused-border off', true)
   else
-    hs.notify.show('Switched KWM modes', '', 'Switched to bsp window mode',''):withdraw()
+    hs.notify.show('Switched KWM modes', '', 'Switched to bsp window mode','')
     hs.execute('kwmc space -t bsp', true)
     hs.execute('kwmc config focused-border on', true)
   end
