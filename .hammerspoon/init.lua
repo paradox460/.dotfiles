1272a1ef90501b53813eb1687338adf21867333a
Jeff Sandberg <jeffsandberg@Jeffs-MacBook-Pro.local>
2016-03-31 13:49:49 -0700

    Removed window mode switching

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -2,18 +2,3 @@ require 'expandmode'
 require 'movemode'
 
 local super = {'ctrl', 'alt', 'cmd', 'shift'}
-
-function screensChangedCallback()
-  local screens = hs.screen.allScreens()
-  if #screens < 2 then
-    hs.notify.show('Switched KWM modes', '', 'Switched to floating window mode','')
-    hs.execute('kwmc space -t float', true)
-    hs.execute('kwmc config focused-border off', true)
-  else
-    hs.notify.show('Switched KWM modes', '', 'Switched to bsp window mode','')
-    hs.execute('kwmc space -t bsp', true)
-    hs.execute('kwmc config focused-border on', true)
-  end
-end
-
-hs.screen.watcher.new(screensChangedCallback):start()
