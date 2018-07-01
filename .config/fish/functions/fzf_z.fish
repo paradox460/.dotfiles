5fa0568e1c4ed70f6432bd2e11f580e51680f56f
Jeff Sandberg <paradox460@gmail.com>
2018-07-01 10:35:29 -0700

    Suppress unwanted output from count check in z

--- .config/fish/functions/fzf_z.fish
+++ .config/fish/functions/fzf_z.fish
@@ -1,12 +1,6 @@
 # # fasd & fzf change directory - jump using `fasd` if given argument, filter output of `fasd` using `fzf` else
-# z() {
-#     [ $# -gt 0 ] && fasd_cd -d "$*" && return
-#     local dir
-#     dir="$(fasd -Rdl "$1" | fzf -1 -0 --no-sort +m)" && cd "${dir}" || return 1
-# }
-
 function fzf_z -d "Fasd + fzf change directory, without arguments pulls up picker, with jumps directly"
-  if count $argv
+  if count $argv > /dev/null
     fasd_cd -d $argv
   else
     fasd -Rdl "" | eval (__fzfcmd) --prompt="fzf\> " -1 -0 +m --no-sort $FZF_Z_OPTS | read -l result
