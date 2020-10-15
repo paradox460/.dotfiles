39ddaa8ff72e6664b46763a3bc30a5ab91abab3d
Jeff Sandberg <paradox460@gmail.com>
2020-10-15 11:44:52 -0600

    Remove obsolete fish commands
    prepend_sudo is built into fish now, and the others are irrelevant as
    abbreviations have taken their place and I don't write much ruby these
    days

--- .config/fish/functions/paradox_binding.fish
+++ .config/fish/functions/paradox_binding.fish
@@ -1,10 +1,6 @@
 # Deal with the hackishness needed to use custom binds in addition to vi mode
 function paradox_binding
   fish_vi_key_bindings
-  bind --mode insert \es paradox_prepend_sudo
-  bind -M insert \ex paradox_prepend_bundle_exec
-  bind -M insert \eg paradox_prepend_git
-  bind -M insert \er paradox_prepend_rbl
 
   if command -sq fzf
     bind -M insert \ez fzf_z
--- .config/fish/functions/paradox_prepend_git.fish
+++
@@ -1,4 +0,0 @@
-function paradox_prepend_git
-  set current_command (commandline)
-  commandline "git $current_command"
-end
