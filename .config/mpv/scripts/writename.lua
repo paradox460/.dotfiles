e3389f278eaa0383c7a9d93cbc264ba88556878c
Jeff Sandberg <paradox460@gmail.com>
2020-11-12 15:51:14 -0700

    Update writename script to also write failed loads
    Can be useful for cleaning out a directory of corrupt media files as you
    watch through them

--- .config/mpv/scripts/writename.lua
+++ .config/mpv/scripts/writename.lua
@@ -1,10 +1,23 @@
--- require "mp.msg"
-function write_name()
-  file = io.open("watched", "a+")
+require "mp.msg"
+function write_name(filename)
+  file = io.open(filename, "a+")
+  path = mp.get_property("path")
   file:write(mp.get_property("path"), "\n")
   mp.command("playlist-next force")
+  mp.msg.info("Wrote " .. path .. " to " .. filename)
   file:flush()
   file:close()
 end
 
-mp.add_key_binding(nil, "write_name", write_name)
+function watched()
+  write_name("watched")
+end
+
+function failed()
+
+  write_name("failed")
+end
+
+mp.add_key_binding(nil, "write_name", watched)
+
+mp.add_hook("on_load_fail", 50, failed)
