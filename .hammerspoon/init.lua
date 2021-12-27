131f5be1b13a52229f41e164bddd381696ee1537
Jeff Sandberg <paradox460@gmail.com>
2021-12-26 19:59:46 -0700

    [hs] Make cli install more robust

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -3,6 +3,12 @@ require 'movemode'
 
 require 'hs.alert'
 require "hs.ipc"
-hs.ipc.cliInstall()
+if not hs.ipc.cliInstall() then
+  print('CLI load failed, trying cleanup and load')
+  hs.ipc.cliUninstall()
+  if hs.ipc.cliInstall() then
+    print('CLI load succeeded!') 
+  end
+end
 
 local super = {'ctrl', 'alt', 'cmd', 'shift'}
