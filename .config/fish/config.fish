8d56cb10583c0b1b085343233b39ce33a9dd40b7
Jeff Sandberg <paradox460@gmail.com>
2022-06-21 16:13:51 -0600

    [🐟🍺] Remove old homebrew completions fix
    This hasn't been needed in almost half a decade

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,11 +7,6 @@ switch (uname -m)
     set brew_root /usr/local
 end
 
-# Completion paths (homebrew fix)
-set fish_complete_path ~/.config/fish/completions/ \
-  /usr/local/share/fish/completions/ \
-  /usr/local/share/fish/vendor_completions.d/ \
-  $fish_complete_path
 source (command $brew_root/bin/brew shellenv | psub)
 
 fish_add_path -P $HOME/.bin $HOME/.dotfiles/.bin $HOME/.cargo/bin
