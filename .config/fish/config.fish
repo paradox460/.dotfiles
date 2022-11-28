964ef2f62d35a72e3a3ce6f4bd4bfeffe37ada0a
Jeff Sandberg <paradox460@gmail.com>
2022-11-28 11:23:41 -0700

    [🐟] Clean up fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -24,10 +24,6 @@ set -gx PAGER "less -FRX"
 # Elixir Options
 set -gx ERL_AFLAGS "-kernel shell_history enabled"
 
-if command -sq fzf && type -q fzf_configure_bindings
-  fzf_configure_bindings --directory=\ct
-end
-
 # ripgrep config
 set -gx RIPGREP_CONFIG_PATH $HOME/.ripgreprc
 
@@ -35,32 +31,52 @@ set -gx RIPGREP_CONFIG_PATH $HOME/.ripgreprc
 # Don't need docs, I have dash
 set -gx RUST_WITHOUT rust-docs
 
-# Alias zone
-# Doesn't really reload, more of launches a new shell, but the results the same
-alias reload 'exec fish -l'
-# Git
-# Hub is a useful tool that provides github integration. If you don't use it, I recommend deleting these lines
-alias git hub
+# iTerm 2 fish integration
+test -e ~/.iterm2_shell_integration.fish ; and source ~/.iterm2_shell_integration.fish
 
-# Up
-alias u='fzf_up'
+# # GPG stuff
+# test -e ~/.config/fish/gnupg.fish ; and source ~/.config/fish/gnupg.fish
 
-# CDHist
-alias cdh='fzf_cdhist'
+# asdf
+test -e ~/.asdf/asdf.fish ; and source ~/.asdf/asdf.fish
 
-# Nvim all the time
-alias vim='nvim'
+# # Direnv
+test -e /usr/local/bin/direnv ; and eval (direnv hook fish)
 
 
-# Get duration of file via ffprobe
-alias ff_duration='ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1'
-
-# Fundle fish plugin manager
-test -e ~/.config/fish/fundle_config.fish ; and source ~/.config/fish/fundle_config.fish
+# Zoxide
+zoxide init fish | source
 
 # Interactive mode settings
 # Specifically, anything you might not need in scripting should be put here.
 if status --is-interactive
+  if command -sq fzf && type -q fzf_configure_bindings
+    fzf_configure_bindings --directory=\ct
+  end
+
+  # Alias zone
+  # Doesn't really reload, more of launches a new shell, but the results the same
+  alias reload 'exec fish -l'
+  # Git
+  # Hub is a useful tool that provides github integration. If you don't use it, I recommend deleting these lines
+  alias git hub
+
+  # Up
+  alias u='fzf_up'
+
+  # CDHist
+  alias cdh='fzf_cdhist'
+
+  # Nvim all the time
+  alias vim='nvim'
+
+
+  # Get duration of file via ffprobe
+  alias ff_duration='ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1'
+
+  # Fundle fish plugin manager
+  test -e ~/.config/fish/fundle_config.fish ; and source ~/.config/fish/fundle_config.fish
+
   # Greeting on new terminal
   set fish_greeting (date)
 
@@ -72,36 +88,11 @@ if status --is-interactive
   abbr --add gc "git checkout"
   abbr --add gcb "git checkout -b"
   abbr --add gu "gh repo sync"
-end
 
-# iTerm 2 fish integration
-test -e ~/.iterm2_shell_integration.fish ; and source ~/.iterm2_shell_integration.fish
-
-# GPG stuff
-test -e ~/.config/fish/gnupg.fish ; and source ~/.config/fish/gnupg.fish
-
-# asdf
-test -e ~/.asdf/asdf.fish ; and source ~/.asdf/asdf.fish
-
-# Homebrew command not found
-# brew command command-not-found-init > /dev/null 2>&1; and source (brew command-not-found-init)
-
-# Direnv
-test -e /usr/local/bin/direnv ; and eval (direnv hook fish)
-
-# env_parallel for gnu parallel
-# Disabling temporarily because it seems broken right now
-# test -e /usr/local/bin/env_parallel.fish ; and source (which env_parallel.fish)
-
-
-set -gx BASE16_THEME_DEFAULT "tomorrow-night"
-# Base16 Shell
-if status --is-interactive
+  # Base16 colorschemes
+  set -gx BASE16_THEME_DEFAULT "tomorrow-night"
   set BASE16_SHELL_PATH "$HOME/.config/base16-shell"
   if test -s "$BASE16_SHELL_PATH"
     source "$BASE16_SHELL_PATH/profile_helper.fish"
   end
 end
-
-# Zoxide
-zoxide init fish | source
