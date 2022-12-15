6f7ae5f0b9dba68ac5022220f77365f616d46553
Jeff Sandberg <paradox460@gmail.com>
2022-12-14 23:18:46 -0700

    Update ffmpeg tomp4 script to fix size issue
    MP4 files must have even width and heights. Doing a simple bit of math
    ensures we have such

--- .config/fish/functions/tomp4.fish
+++ .config/fish/functions/tomp4.fish
@@ -14,7 +14,7 @@ function tomp4 --description "Convert video file(s) to MP4 suitible for Telegram
   for i in $argv
     set -l ext (string split --right --max 1 '.' -- $i)[-1]
     set -l newname (basename $i "."$ext)'.mp4'
-    ffmpeg -hide_banner -i $i -c:v libx264 -c:a aac -pix_fmt yuv420p -map_metadata -1 $newname
+    ffmpeg -hide_banner -i $i -c:v libx264 -c:a aac -pix_fmt yuv420p -map_metadata -1 -vf "scale=ceil(iw/2)*2:ceil(ih/2)*2" $newname
     set -q _flag_copy; and file-to-clipboard (readlink -f $newname)
   end
 end
