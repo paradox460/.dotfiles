5af3bbf9bcc9cc36cc745d64b1caf0d4e6887c73
Jeff Sandberg <paradox460@gmail.com>
2023-01-09 11:19:31 -0700

    [🐟]Rewrite fish config, faster and smaller

---
+++ .config/fish/conf.d/abbreviations.fish
@@ -0,0 +1,14 @@
+  abbr --add g "git"
+  abbr --add gst "git status"
+  abbr --add gcm "git commit -m"
+  abbr --add gc "git checkout"
+  abbr --add gcb "git checkout -b"
+  abbr --add gu "gh repo sync"
+
+function multicd
+    echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
+end
+abbr --add dotdot --regex '^\.\.+$' --function multicd
+
+function last_history_item; echo $history[1]; end
+abbr -a !! --position anywhere --function last_history_item
