85bf4459175d9eeab1d199334cd43ccb373541c9
Jeff Sandberg <paradox460@gmail.com>
2023-09-04 02:01:22 -0600

    Move from Goku To Karabiner.ts
    Typescript, and composed configuration, is preferable to the somewhat
    loose lisp-y edn used by Goku. I like lisp, but don't write it enough to
    not trip over ParEdit and friends.

---
+++ karabiner-config/src/index.ts
@@ -0,0 +1,32 @@
+import {
+  DeviceIdentifier,
+  ModifierParam,
+  map as from,
+  ifApp,
+  ifDevice,
+  rule,
+  withCondition,
+  writeToProfile
+} from 'karabiner.ts';
+
+const footPedal: DeviceIdentifier = { product_id: 45057, vendor_id: 13651 };
+
+writeToProfile('Default', [
+
+  rule("Caps-lock to escape when alone, hyper when used as modifier, caps-lock when used with Option").manipulators([
+    from("caps_lock", "left_option").to("caps_lock"),
+    from("caps_lock").toHyper().toIfAlone("escape")
+  ]),
+
+  rule("Foot Pedals", ifDevice(footPedal)).manipulators([
+    withCondition(ifApp({ file_paths: ["^/opt/homebrew/bin/mpv$"] }))([
+      from("a").to("rewind"),
+      from("b").to("play_or_pause"),
+      from("c").to("fastforward")
+    ]),
+    from("a", "left_option").to("caps_lock"),
+    from("a").toHyper().toIfAlone("escape"),
+    from("b").to("m", "Hyper"),
+    from("c").to("left_command")
+  ])
+])
