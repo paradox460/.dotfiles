31d2d69f3c117b3a243a7ea03bdf9486b73a4c46
Jeff Sandberg <jeffrey.sandberg@thescore.com>
2025-03-31 12:29:02 -0600

    Add mute toggle button for teams focus

--- karabiner.ts
+++ karabiner.ts
@@ -9,7 +9,7 @@ import {
   rule,
   withCondition,
   writeToProfile
-} from 'https://deno.land/x/karabinerts@1.25.0/deno.ts';
+} from 'https://deno.land/x/karabinerts@1.31.0/deno.ts';
 
 const footPedal: DeviceIdentifier = { product_id: 45057, vendor_id: 13651 };
 
@@ -25,6 +25,9 @@ writeToProfile('Default', [
       from("a").to("rewind"),
       from("b").to("play_or_pause"),
       from("c").to("fastforward")
+    ]),
+    withCondition(ifApp( { bundle_identifiers: [/^com.microsoft.teams2$/]}))([
+      from("c").to("m", ["left_command", "left_shift"])
     ]),
     from("a", "left_option").to("caps_lock"),
     from("a").toHyper().toIfAlone("escape"),
